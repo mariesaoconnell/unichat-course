6c1e3b9ad2ff9bac4b16e2cfd520433b1e202daf
mariesaoconnell <mariesa.oconnell@gmail.com>
2022-10-15 18:48:10 -0400

    add context and add chats

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,23 @@ import React from "react"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-// import { AuthProvider } from "../contexts/AuthContext"
+import { AuthProvider } from "../contexts/AuthContext";
 
-// import Chats from "./Chats"
+import Chats from "./Chats"
 import Login from "./Login"
 
 function App() {
   return (
-    <div style={{ fontFamily: 'Avenir' }}>
-      <Router>
-        {/* <AuthProvider> */}
-
-          {/* Switch will render what the user sees now, login vs chats */}
-          <Switch>
-            {/* <Route path="/chats" component={Chats} /> */}
-            <Route path="/" component={Login} />
-          </Switch>
-        {/* </AuthProvider> */}
-      </Router>
-    </div>
-  )
+		<div style={{ fontFamily: 'Avenir' }}>
+			<Router>
+				<AuthProvider>
+					<Switch> {/* Switch will render what the user sees now, login vs chats */}
+						<Route path='/' component={Login} /> {/* <Route path="/chats" component={Chats} /> */}
+					</Switch>
+				</AuthProvider>
+			</Router>
+		</div>
+	);
 }
 
 export default App
